6b823f89d0a29d6850e0ad7d7423e0235d186654
nawrazaltai <nawraz.altai@chasacademy.se>
2023-04-11 20:50:31 +0200

    backend with mySQL

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,18 @@ import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import mysql from "mysql";
 
 const app = express();
 const PORT = 4000;
 dotenv.config();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "ninja",
+  password: "123456",
+  database: "bankjwt",
+});
 
 app.use(
   cors({
@@ -25,51 +33,107 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.post("/users", (req, res) => {
-  console.log(req.body);
-  let userId = uuidv4();
-  let username = req.body.username;
-  const user = {
-    id: userId,
-    username: username,
-    password: req.body.password,
-  };
-  users.push(user);
+  //   console.log(req.body);
+  //   let userId = uuidv4();
+  //   let username = req.body.username;
+  //   const user = {
+  //     id: userId,
+  //     username: username,
+  //     password: req.body.password,
+  //   };
+  //   users.push(user);
 
-  let balance = req.body.balance;
-  const account = {
-    id: count,
-    userId: userId,
-    balance: balance,
-  };
-  accounts.push(account);
-  count += 1;
-  res.json({ user, account });
+  //   let balance = req.body.balance;
+  //   const account = {
+  //     id: count,
+  //     userId: userId,
+  //     balance: balance,
+  //   };
+  //   accounts.push(account);
+  //   count += 1;
+  //   res.json({ user, account });
+
+  const user = req.body;
+  const { username, password, balance } = user;
+
+  connection.query(
+    "INSERT INTO users (username, password) VALUES (?, ?)",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        const userId = result.insertId;
+        connection.query(
+          "INSERT INTO accounts (user_id, balance) VALUES (?, ?)",
+          [userId, balance],
+          (err, result) => {
+            if (err) {
+              res.sendStatus(500);
+            } else {
+              res.send({ message: "OK" });
+            }
+          }
+        );
+      }
+    }
+  );
 });
 
 function generateAccessToken(username) {
   return jwt.sign(username.id, process.env.ACCESS_TOKEN_SECRET);
 }
 
+// app.post("/sessions", (req, res) => {
+//   //   console.log("sessions");
+//   const userInput = req.body;
+
+//   const user = users.find((u) => u.username == userInput.usernameInput);
+
+//   if (user.password == userInput.passwordInput) {
+//     const token = generateAccessToken(user);
+
+//     res.cookie("token", token, {
+//       httpOnly: true,
+//       secure: false,
+//       SameSite: "none",
+//       credentials: true,
+//       //   authorization: "Bearer" + token,
+//     });
+//     res.json({ token, user });
+//   } else {
+//     res.json("fail");
+//   }
+// });
+
 app.post("/sessions", (req, res) => {
   //   console.log("sessions");
   const userInput = req.body;
+  const { usernameInput, passwordInput } = userInput;
 
-  const user = users.find((u) => u.username == userInput.usernameInput);
+  connection.query(
+    "SELECT * FROM users WHERE (username, password) = (?, ?)",
+    [usernameInput, passwordInput],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const user = result[0];
+        // req.user = user;
+        const token = generateAccessToken(user);
 
-  if (user.password == userInput.passwordInput) {
-    const token = generateAccessToken(user);
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          SameSite: "none",
+          credentials: true,
+          //   authorization: "Bearer" + token,
+        });
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      SameSite: "none",
-      credentials: true,
-      //   authorization: "Bearer" + token,
-    });
-    res.json({ token, user });
-  } else {
-    res.json("fail");
-  }
+        res.json({ user, token });
+      }
+    }
+  );
 });
 
 function authenticateToken(req, res, next) {
@@ -86,16 +150,52 @@ function authenticateToken(req, res, next) {
   });
 }
 
+// app.get("/me/accounts", authenticateToken, (req, res) => {
+//   const userId = req.userId;
+//   // res.cookie = req.cookies["token"]
+
+//   accounts.map((account) => {
+//     if (account.userId == userId) {
+//       const user = users.find((user) => user.id == userId);
+//       res.json({ balance: account.balance, username: user.username });
+//     }
+//   });
+// });
+
 app.get("/me/accounts", authenticateToken, (req, res) => {
   const userId = req.userId;
+  //   console.log(req.user);
+  connection.query(
+    "SELECT * FROM users WHERE id = ?",
+    [userId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const username = result[0].username;
+        connection.query(
+          "SELECT balance FROM accounts WHERE user_id = ?",
+          [userId],
+          (err, result) => {
+            if (err) {
+              console.log(err);
+            } else {
+              const balance = result[0].balance;
+              res.json({ username, balance });
+            }
+          }
+        );
+      }
+    }
+  );
   // res.cookie = req.cookies["token"]
 
-  accounts.map((account) => {
-    if (account.userId == userId) {
-      const user = users.find((user) => user.id == userId);
-      res.json({ balance: account.balance, username: user.username });
-    }
-  });
+  //   accounts.map((account) => {
+  //     if (account.userId == userId) {
+  //       const user = users.find((user) => user.id == userId);
+  //       res.json({ balance: account.balance, username: user.username });
+  //     }
+  //   });
 });
 
 app.post("/logout", (req, res) => {
@@ -104,6 +204,13 @@ app.post("/logout", (req, res) => {
   res.send({ message: "ok" });
 });
 
-app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT, () => {
+      console.log("DB Connected");
+      console.log("Server started on port " + PORT);
+    });
+  }
 });
